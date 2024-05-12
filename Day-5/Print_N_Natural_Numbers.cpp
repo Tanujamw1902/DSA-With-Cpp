29aebba621d4bb3f852c727609ba8bf049aa2870
Tanuja Waware <161374113+Tanujamw1902@users.noreply.github.com>
2024-05-12 10:47:16 +0530

    Day 5

---
+++ Day-5/Print_N_Natural_Numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter your n: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for(int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
