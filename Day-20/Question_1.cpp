b6653a7c3b0d92a26b07caa6a6c8957eff16b62e
Tanuja Waware <161374113+Tanujamw1902@users.noreply.github.com>
2024-06-29 00:31:46 +0530

    Day 20

---
+++ Day-20/Question_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int binarySearch(int arr[], int startIndex, int endIndex, int key){
+    if(startIndex > endIndex){
+        return -1;
+    }
+    int mid = startIndex + (endIndex - startIndex) / 2;
+    if(arr[mid] == key){
+        return mid;
+    }else if(arr[mid] > key){
+        return binarySearch(arr, startIndex, mid - 1, key);
+    }else{
+        return binarySearch(arr, mid + 1, endIndex, key);
+    }
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 7;
+
+    int result = binarySearch(arr, 0, n - 1, key);
+    if (result != -1){
+        cout << "Key is found at this index in the array: " << result << endl;
+    }else{
+        cout << "Key is not founf at this index in this array: " << result << endl;
+    }
+    
+    return 0;
+}
